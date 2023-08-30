3b300befab254758ed1bdcc5aef72bb7ac0c0860
ExiNni <86674480+ExiNni@users.noreply.github.com>
2023-08-30 18:41:23 +0900

    variable

--- chap01_Variable/src/com/kh/practice1/func/VariablePractice3.java
+++ chap01_Variable/src/com/kh/practice1/func/VariablePractice3.java
@@ -14,7 +14,7 @@ public class VariablePractice3 {
 		
 		System.out.print("세로 길이를 입력해주세요: ");
 		height = sc.nextDouble();
-		
+		  
 		System.out.println("면적의 넓이는 " + (width*height) + " 이며 둘레의 길이는 " + 
 		(2*(width+height)) + " 입니다");
 		
