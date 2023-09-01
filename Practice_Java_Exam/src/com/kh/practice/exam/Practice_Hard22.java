53a1d3085eb435a9a5da44c039581df57d464d2f
ExiNni <86674480+ExiNni@users.noreply.github.com>
2023-09-01 10:39:27 +0900

    for while

--- Practice_Java_Exam/src/com/kh/practice/exam/Practice_Hard22.java
+++ Practice_Java_Exam/src/com/kh/practice/exam/Practice_Hard22.java
@@ -5,41 +5,38 @@ import java.util.Scanner;
 public class Practice_Hard22 {
 
 	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-        System.out.print("정수를 입력해주세요: ");
-        int num = sc.nextInt();
+		System.out.print("정수를 입력해주세요: ");
+		int num = sc.nextInt();
 
-        for(int i = 1; i <= num; i++) {
-        	if( i == 1) {
-        		for(int j = 1; j <= num; j++) {
-        			System.out.print("*");
-        			
-        		}System.out.println("");
-        	}else if(i == num) {
-        		for(int k = 1; k <= num; k++) {
-        			System.out.print("*");
-        		}
-        		
-        	}else {for(int l = i; l <= num ; l++) {
-    			if(l == i + 1) {
-    				System.out.print("*");
-    				for(int m = l; m <= num ; m++) {
-    					System.out.print(" ");
-    				}
-    				System.out.println("*");
-    			}
-    			
-    		}
-        	
-    		
-        	
-    			
-    		}
-        		
-        }
+		for (int i = 1; i <= num; i++) {
+			if (i == 1) {
+				for (int j = 1; j <= num; j++) {
+					System.out.print("*");
+
+				}
+				System.out.println("");
+			} else if (i == num) {
+				for (int k = 1; k <= num; k++) {
+					System.out.print("*");
+				}
+
+			} else {
+				for (int l = i; l <= num; l++) {
+					if (l == i + 1) {
+						System.out.print("*");
+						for (int m = l; m <= num; m++) {
+							System.out.print(" ");
+						}
+						System.out.println("*");
+					}
+
+				}
+
+			}
+
+		}
 	}
-	
+
 }
-
-
